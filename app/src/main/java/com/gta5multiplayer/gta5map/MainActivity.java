63797fea343377fa7820cf6a065c0581153206bc
Gflow <Gflow>
2017-04-03 02:33:49 +0300

    Initial commit

---
+++ app/src/main/java/com/gta5multiplayer/gta5map/MainActivity.java
@@ -0,0 +1,161 @@
+package com.gta5multiplayer.gta5map;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "MainActivity";
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        WebView browser = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = browser.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        browser.loadUrl("file:///android_asset/interactive/map.html");
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.ammunation) {
+            // Handle the camera action
+        } else if (id == R.id.atm) {
+
+        } else if (id == R.id.barber) {
+
+        } else if (id == R.id.carwash) {
+
+        } else if (id == R.id.cinema) {
+
+        } else if (id == R.id.clothing) {
+
+        } else if (id == R.id.conveniencestore) {
+
+        } else if (id == R.id.darts) {
+
+        } else if (id == R.id.epsilontract) {
+
+        } else if (id == R.id.firestation) {
+
+        } else if (id == R.id.flightschool) {
+
+        } else if (id == R.id.foodanddrinks) {
+
+        } else if (id == R.id.golf) {
+
+        } else if (id == R.id.helipad) {
+
+        } else if (id == R.id.hunting) {
+
+        } else if (id == R.id.knifeflight) {
+
+        } else if (id == R.id.letterscrap) {
+
+        } else if (id == R.id.lscustoms) {
+
+        } else if (id == R.id.nuclearwaste) {
+
+        } else if (id == R.id.parachuting) {
+
+        } else if (id == R.id.race) {
+
+        } else if (id == R.id.shootingrange) {
+
+        } else if (id == R.id.spaceshippart) {
+
+        } else if (id == R.id.strangersandfreaks) {
+
+        } else if (id == R.id.stripclub) {
+
+        } else if (id == R.id.stuntlocation) {
+
+        } else if (id == R.id.submarineparts) {
+
+        } else if (id == R.id.tattoo) {
+
+        } else if (id == R.id.tennis) {
+
+        } else if (id == R.id.triathlon) {
+
+        } else if (id == R.id.underthebridge) {
+
+        } else if (id == R.id.yoga) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
